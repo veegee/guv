0de8324ecd1662d49afb6ab838c680b905c2271a
veegee <veegee@veegee.org>
2014-11-23 14:08:59 -0500

    Add cProfile output

--- examples/guv_simple_http_response.py
+++ examples/guv_simple_http_response.py
@@ -2,8 +2,11 @@ import guv
 
 guv.monkey_patch()
 import guv.server
+import guv.hubs
 import logging
 import time
+import cProfile
+import pstats
 
 from util import create_example
 import logger
@@ -34,4 +37,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    hub_name = guv.hubs.get_hub().__module__.split('.')[2]
+    profile_fname = 'guv_{}.profile'.format(hub_name)
+    stats_fname = 'guv_{}.stats'.format(hub_name)
+
+    cProfile.run('main()', profile_fname)
+
+    p = pstats.Stats(profile_fname)
+    stats = p.strip_dirs().sort_stats('time', 'cumulative')
+    stats.print_stats(40)
+    stats.dump_stats(stats_fname)
