6ffe2085d4509a52173836addafbe804201c43b2
veegee <veegee@veegee.org>
2014-11-22 22:52:28 -0500

    Create pyuv_cffi example script

---
+++ examples/pyuv_cffi_example_1.py
@@ -0,0 +1,36 @@
+from . import Loop, Timer, Signal
+
+
+def sig_cb(sig_h, sig_num):
+    print('\nsig_cb({}, {})'.format(sig_h, sig_num))
+    sig_h.stop()
+    sig_h.loop.stop()
+
+
+def timer_cb(timer_h):
+    print('timer_cb({})'.format(timer_h))
+
+
+def run():
+    import signal
+
+    loop = Loop()
+
+    timer_h = Timer(loop)
+    timer_h.start(timer_cb, 1, 1)
+
+    sig_h = Signal(loop)
+    sig_h.start(sig_cb, signal.SIGINT)
+
+    status = loop.run()
+
+    timer_h.close()  # stop and free any timers before freeing the loop
+    print('loop.run() -> ', status)
+
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    main()
