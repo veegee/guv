ffe6219145c2791c1ed618a6656a0fa9d72e75d3
veegee <veegee@veegee.org>
2014-11-27 19:08:41 -0500

    Move errors to exceptions.py

--- guv/exceptions.py
+++ guv/exceptions.py
@@ -1,2 +1,32 @@
+from errno import (EWOULDBLOCK, EINPROGRESS, EALREADY, EISCONN, EBADF, ENOTCONN, ESHUTDOWN, EAGAIN,
+                   ECONNRESET, EPIPE, EINVAL, ECONNABORTED)
+
+from . import patcher
+
+osocket = patcher.original('socket')
+
+from .support import OS_WINDOWS
+
+if OS_WINDOWS:
+    # winsock sometimes throws ENOTCONN
+    SOCKET_BLOCKING = {EAGAIN, EWOULDBLOCK}
+    SOCKET_CLOSED = {ECONNRESET, ESHUTDOWN, ENOTCONN}
+    CONNECT_ERR = {EINPROGRESS, EALREADY, EWOULDBLOCK, EINVAL}
+else:
+    # oddly, on linux/darwin, an unconnected socket is expected to block,
+    # so we treat ENOTCONN the same as EWOULDBLOCK
+    SOCKET_BLOCKING = {EAGAIN, EWOULDBLOCK, ENOTCONN}
+    SOCKET_CLOSED = {ECONNRESET, ESHUTDOWN, EPIPE}
+    CONNECT_ERR = {EINPROGRESS, EALREADY, EWOULDBLOCK}
+
+CONNECT_SUCCESS = {0, EISCONN}
+
+BAD_SOCK = {EBADF, ECONNABORTED}
+BROKEN_SOCK = {EPIPE, ECONNRESET}
+
+ACCEPT_EXCEPTIONS = {osocket.error}
+ACCEPT_ERRNO = {EPIPE, EBADF, ECONNRESET}
+
+
 class IOClosed(IOError):
     pass
--- guv/greenio.py
+++ guv/greenio.py
@@ -3,27 +3,11 @@ import time
 import os
 import _socket
 import errno
-from errno import (EWOULDBLOCK, EINPROGRESS, EALREADY, EISCONN, EBADF, ENOTCONN, ESHUTDOWN, EAGAIN,
-                   ECONNRESET, EPIPE, EINVAL)
+from errno import (EWOULDBLOCK, EBADF, EAGAIN)
 
 from . import patcher
 from .hubs import trampoline
-from .exceptions import IOClosed
-from .support import OS_WINDOWS
-
-if OS_WINDOWS:
-    # winsock sometimes throws ENOTCONN
-    SOCKET_BLOCKING = {EAGAIN, EWOULDBLOCK}
-    SOCKET_CLOSED = {ECONNRESET, ESHUTDOWN, ENOTCONN}
-    CONNECT_ERR = {EINPROGRESS, EALREADY, EWOULDBLOCK, EINVAL}
-else:
-    # oddly, on linux/darwin, an unconnected socket is expected to block,
-    # so we treat ENOTCONN the same as EWOULDBLOCK
-    SOCKET_BLOCKING = {EAGAIN, EWOULDBLOCK, ENOTCONN}
-    SOCKET_CLOSED = {ECONNRESET, ESHUTDOWN, EPIPE}
-    CONNECT_ERR = {EINPROGRESS, EALREADY, EWOULDBLOCK}
-
-CONNECT_SUCCESS = {0, EISCONN}
+from .exceptions import IOClosed, SOCKET_BLOCKING, SOCKET_CLOSED, CONNECT_ERR, CONNECT_SUCCESS
 
 osocket = patcher.original('socket')
 AF_INET = osocket.AF_INET
