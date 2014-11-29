2367a5454660ffec6b4bab11939edc52197be791
veegee <veegee@veegee.org>
2014-11-28 20:54:44 -0500

    Reorganize imports (and bump version)

--- guv/__init__.py
+++ guv/__init__.py
@@ -1,41 +1,18 @@
-version_info = (0, 20, 0)
+version_info = (0, 21, 0)
 __version__ = '.'.join(map(str, version_info))
 
 try:
-    from . import greenthread
     from . import greenpool
     from . import queue
-    from . import timeout
-    from . import patcher
-    from . import server
     from .hubs.trampoline import gyield
-    import greenlet
+    from .greenthread import sleep, spawn, spawn_n, spawn_after, kill
+    from .greenpool import GreenPool, GreenPile
+    from .timeout import Timeout, with_timeout
+    from .patcher import import_patched, monkey_patch
+    from .server import serve, listen, connect, StopServe, wrap_ssl
+
     import pyuv_cffi  # only to compile the shared library before monkey-patching
 
-    sleep = greenthread.sleep
-    spawn = greenthread.spawn
-    spawn_n = greenthread.spawn_n
-    spawn_after = greenthread.spawn_after
-    kill = greenthread.kill
-
-    Timeout = timeout.Timeout
-    with_timeout = timeout.with_timeout
-
-    GreenPool = greenpool.GreenPool
-    GreenPile = greenpool.GreenPile
-
-    Queue = queue.Queue
-
-    import_patched = patcher.import_patched
-    monkey_patch = patcher.monkey_patch
-
-    serve = server.serve
-    listen = server.listen
-    connect = server.connect
-    StopServe = server.StopServe
-    wrap_ssl = server.wrap_ssl
-
-    getcurrent = greenlet.greenlet.getcurrent
 except ImportError as e:
     # This is to make Debian packaging easier, it ignores import errors of greenlet so that the
     # packager can still at least access the version. Also this makes easy_install a little quieter
