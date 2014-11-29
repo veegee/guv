f81f5d0790f2dfdd479e05f0f8a877ae48de1811
veegee <veegee@veegee.org>
2014-11-29 17:34:38 -0500

    Delete unused module
    We can put this back in when we need it.

--- guv/green/ftplib.py
+++
@@ -1,9 +0,0 @@
-from .. import patcher
-
-# *NOTE: there might be some funny business with the "SOCKS" module
-# if it even still exists
-from ..green import socket
-
-patcher.inject('ftplib', globals(), ('socket', socket))
-
-del patcher
