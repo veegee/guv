e5803617b27144cb88563b3533b66f0b96482143
veegee <veegee@veegee.org>
2014-11-29 17:19:30 -0500

    Declare sleep as a global instead of relying on import
    This is a nicer way to define it in the greenified module. Unused imports may
    accidentally disappear after using your IDE's "optimize imports" function.

--- guv/green/time.py
+++ guv/green/time.py
@@ -1,8 +1,13 @@
+"""Greenified :mod:`time` module
+
+The only thing that needs to be patched from :mod:`time` is :func:`time.sleep` to yield instead
+of block the thread.
+"""
 __time = __import__('time')
 from ..patcher import slurp_properties
 
 __patched__ = ['sleep']
 slurp_properties(__time, globals(), ignore=__patched__, srckeys=dir(__time))
-from ..greenthread import sleep
+from .. import greenthread
 
-sleep  # silence pyflakes
+sleep = greenthread.sleep
