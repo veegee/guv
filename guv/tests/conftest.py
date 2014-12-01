1e8352c7823b154de3d3ee39bfafc19fca3731e3
veegee <veegee@veegee.org>
2014-12-01 02:48:26 -0500

    Add more tests

--- guv/tests/conftest.py
+++ guv/tests/conftest.py
@@ -3,6 +3,21 @@ import pytest
 from ..greenio import socket
 
 
+@pytest.fixture(scope='session')
+def pub_addr():
+    """A working public address that is considered always available
+    """
+    return 'gnu.org', 80
+
+
+@pytest.fixture(scope='session')
+def fail_addr():
+    """An address that nothing is listening on
+    """
+    return '192.0.0.0.0', 1000
+
+
 @pytest.fixture(scope='function')
 def gsock():
     return socket()
+
--- guv/tests/test_greenio.py
+++ guv/tests/test_greenio.py
@@ -1,7 +1,46 @@
-from ..greenio import socket
+import errno
+
+import pytest
+
+from ..green import socket
+from ..greenio import socket as green_socket
 
 
 class TestGreenSocket:
     def test_socket_init(self):
-        sock = socket()
+        sock = socket.socket()
+        assert isinstance(sock, green_socket)
+
+    def test_socket_close(self, gsock):
+        gsock.close()
+
+    def test_connect(self, gsock, pub_addr):
+        gsock.connect(pub_addr)
+        print(gsock.getpeername())
+        assert gsock.getpeername()
+
+        # check that connecting again raises with EISCONN
+        with pytest.raises(OSError) as exc_info:
+            gsock.connect(pub_addr)
+            if hasattr(exc_info, 'value'):
+                assert exc_info.value.args[0] == errno.EISCONN
+
+    def test_connect_timeout(self, gsock, fail_addr):
+        gsock.settimeout(0.01)
+
+        with pytest.raises(OSError):
+            gsock.connect(fail_addr)
+
+    def test_send_to_closed_sock_raises(self, gsock):
+        with pytest.raises(BrokenPipeError):
+            gsock.send(b'hello')
+
+
+class TestGreenModule:
+    def test_create_connection(self, pub_addr):
+        sock = socket.create_connection(pub_addr)
+
+    def test_create_connection_timeout_error(self, fail_addr):
+        with pytest.raises(OSError):
+            sock = socket.create_connection(fail_addr, timeout=0.01)
 
