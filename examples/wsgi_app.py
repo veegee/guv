62e4f4b8262c78a20c26de7b9b23a89d2c2e1e90
veegee <veegee@veegee.org>
2014-12-02 04:15:23 -0500

    Add docstring to WSGI example

--- examples/wsgi_app.py
+++ examples/wsgi_app.py
@@ -1,6 +1,6 @@
 import guv
-
 guv.monkey_patch()
+
 import guv.wsgi
 import logger
 
@@ -8,6 +8,11 @@ logger.configure()
 
 
 def app(environ, start_response):
+    """
+    This is very basic WSGI app useful for testing the performance of guv and guv.wsgi without
+    the overhead of a framework such as Flask. However, it can just as easily be any other WSGI app
+    callable object, such as a Flask or Bottle app.
+    """
     status = '200 OK'
     output = [b'Hello World!']
     content_length = str(len(b''.join(output)))
