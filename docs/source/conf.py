c9f608784ddb3ca221d1106efac95954ef137b09
veegee <veegee@veegee.org>
2014-12-02 21:59:45 -0500

    Add doc preprocessing
    Taken from amqpy. This adds some customization to the generated doc output.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,9 @@
 import sys
 import os
+import re
+import inspect
+
+from docutils import nodes
 
 import sphinx.ext.autodoc
 import sphinx
@@ -60,7 +64,7 @@ html_theme_path = ['../theme']
 html_theme = 'bootstrap'
 html_theme_options = {
     # Tab name for entire site. (Default: "Site")
-    'navbar_site_name': 'guv Docs',
+    'navbar_site_name': 'guv docs',
 
     # Render the next and previous page links in navbar. (Default: true)
     'navbar_sidebarrel': False,
@@ -111,3 +115,145 @@ html_theme_options = {
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'guvdoc'
 
+
+def get_field(doc: str, name: str):
+    """Parse ReST field from document
+
+    :param str doc: string of whole document
+    :param name: name of field excluding the surrounding `:`
+    :return: value of field
+    :rtype: str
+    """
+    match = re.search(':{}: (.*)$'.format(name), doc, re.IGNORECASE | re.MULTILINE)
+    if match:
+        return match.group(1).strip()
+
+
+def autodoc_process_signature(app, what, name, obj, options, signature, return_annotation):
+    doc = str(obj.__doc__)
+    otype = str(type(obj))
+    msg_meth = '{what:11} {otype:23} {name:50} -> {rt}'
+    msg_class = '{what:11} {otype:23} {name:50} {sig}'
+
+    rt = None
+    if what == 'method' and not name.endswith('__init__'):
+        # annotate return type for methods (excluding __init__)
+        if 'rtype' in doc:
+            rt = get_field(doc, 'rtype')
+            print(msg_meth.format(**locals()))
+        elif obj.__doc__ is not None:
+            # assume methods with a docstring but undocumented rtype return `None`
+            rt = 'None'
+            print(msg_meth.format(**locals()))
+        else:
+            # no docstring defined
+            rt = '?'
+            print(msg_meth.format(**locals()))
+    elif type(obj) is property:
+        # annotate return type for properties
+        if 'rtype' in doc:
+            rt = get_field(doc, 'rtype')
+            print(msg_meth.format(**locals()))
+        else:
+            # no docstring defined
+            rt = '?'
+            print(msg_meth.format(**locals()))
+    elif what == 'class':
+        # modify the signature of classes to look like Python code
+        # check base classes
+        sig_items = []
+        for cls in obj.__bases__:
+            if cls.__module__ == 'builtins':
+                name = cls.__name__
+            else:
+                name = '{}.{}'.format(cls.__module__, cls.__name__)
+
+            if name in ['object']:
+                # exclude 'object'
+                continue
+
+            sig_items.append(name)
+
+        # check metaclass
+        try:
+            definition = inspect.getsourcelines(obj)[0][0].strip()
+            if 'metaclass' in definition:
+                cls = type(obj)
+                name = '{}.{}'.format(cls.__module__, cls.__name__)
+                sig_items.append('metaclass={}'.format(name))
+
+        except OSError:
+            pass
+
+        sig_str = ', '.join(sig_items)
+        sig = '({})'.format(sig_str) if sig_str else ''
+
+        print(msg_class.format(**locals()))
+
+        return sig, None
+    else:
+        rt = 'skip'
+        print(msg_meth.format(**locals()))
+
+    if rt and rt not in ['?', 'skip']:
+        return signature, rt
+
+
+def autodoc_process_docstring(app, what, name, obj, options, lines):
+    s_before = None
+    s_after = None
+    if type(obj) == property:
+        s_before = ':annotation:`@property`\n'
+        # this as a @property
+    elif what == 'method' and hasattr(obj, '__isabstractmethod__') and obj.__isabstractmethod__:
+        # this is an @abstractmethod
+        s_before = ':annotation:`@abstractmethod`\n'
+    elif what == 'class' and tuple in obj.__bases__ and hasattr(obj, '_fields'):
+        # this is a namedtuple
+        lines[0] = '**namedtuple** :namedtuple:`{}`'.format(lines[0])
+
+    if s_before:
+        for line in reversed(s_before.split('\n')):
+            lines.insert(0, line)
+    if s_after:
+        for line in s_after.split('\n'):
+            lines.append(line)
+
+
+def autodoc_skip_member(app, what, name, obj, sphinx_skip, options):
+    skip = False
+    doc = obj.__doc__
+    if type(obj) is property:
+        if doc and re.match('Alias for field number \d+$', doc):
+            # this is a namedtuple property
+            skip = True
+
+    return sphinx_skip or skip
+
+
+def generic_span_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    """
+    :param name: role name used in the document
+    :param rawtext: entire markup snippet, with role
+    :param text: text marked with the role
+    :param lineno: line number where rawtext appears in the input
+    :param inliner: inliner instance that called us
+    :param options: directive options for customization
+    :param content: directive content for customization
+    :return: list, list
+    :rtype: list, list
+    """
+    node = nodes.inline(rawtext, text)
+    node['classes'] = [name]
+    return [node], []
+
+
+def setup(app):
+    # register custom ReST roles
+    app.add_role('annotation', generic_span_role)
+    app.add_role('namedtuple', generic_span_role)
+
+    # connect methods to events
+    app.connect('autodoc-process-signature', autodoc_process_signature)
+    app.connect('autodoc-process-docstring', autodoc_process_docstring)
+    app.connect('autodoc-skip-member', autodoc_skip_member)
