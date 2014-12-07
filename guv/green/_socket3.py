2ba1bd0c425c29e9555e638043cc41901c6c1136
veegee <veegee@veegee.org>
2014-12-07 15:47:23 -0500

    Remove GreenSSLObject
    This is not being used anywhere.

--- guv/green/_socket3.py
+++ guv/green/_socket3.py
@@ -3,8 +3,6 @@ import socket as socket_orig
 __all__ = socket_orig.__all__
 __patched__ = ['fromfd', 'socketpair', 'ssl', 'socket']
 
-import ssl
-
 from ..patcher import copy_attributes
 
 copy_attributes(socket_orig, globals(),
@@ -28,52 +26,3 @@ try:
         return socket(one), socket(two)
 except AttributeError:
     pass
-
-
-class GreenSSLObject:
-    """ Wrapper object around the SSLObjects returned by socket.ssl, which have a
-    slightly different interface from SSL.Connection objects. """
-
-    def __init__(self, green_ssl_obj):
-        """ Should only be called by a 'green' socket.ssl """
-        self.connection = green_ssl_obj
-        try:
-            # if it's already connected, do the handshake
-            self.connection.getpeername()
-        except:
-            pass
-        else:
-            try:
-                self.connection.do_handshake()
-            except ssl.SSLSyscallError as e:
-                raise
-
-    def read(self, n=1024):
-        """If n is provided, read n bytes from the SSL connection, otherwise read
-        until EOF. The return value is a string of the bytes read."""
-        try:
-            return self.connection.read(n)
-        except ssl.SSLZeroReturnError:
-            return ''
-        except ssl.SSLSyscallError as e:
-            raise
-
-    def write(self, s):
-        """Writes the string s to the on the object's SSL connection.
-        The return value is the number of bytes written. """
-        try:
-            return self.connection.write(s)
-        except ssl.SSLSyscallError as e:
-            raise
-
-    def server(self):
-        """ Returns a string describing the server's certificate. Useful for debugging
-        purposes; do not parse the content of this string because its format can't be
-        parsed unambiguously. """
-        return str(self.connection.get_peer_certificate().get_subject())
-
-    def issuer(self):
-        """Returns a string describing the issuer of the server's certificate. Useful
-        for debugging purposes; do not parse the content of this string because its
-        format can't be parsed unambiguously."""
-        return str(self.connection.get_peer_certificate().get_issuer())
