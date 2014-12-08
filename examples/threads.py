1b6e3a4b51f0a85e63715984e9b670096d369db7
veegee <veegee@veegee.org>
2014-12-07 23:37:04 -0500

    Add calls to gyield() to ensure that we're in the same OS thread

--- examples/threads.py
+++ examples/threads.py
@@ -2,6 +2,7 @@ import guv
 
 guv.monkey_patch()
 
+from guv import gyield, sleep
 import threading
 import greenlet
 
@@ -16,13 +17,26 @@ def f():
     greenlet_ids[1] = greenlet.getcurrent()
     debug(2)
 
+    print('t: 1')
+    gyield()
+    print('t: 2')
+    gyield()
+    print('t: 3')
+
 
 def main():
     greenlet_ids[0] = greenlet.getcurrent()
     debug(1)
+
     t = threading.Thread(target=f)
     t.start()
     debug(3)
+
+    print('m: 1')
+    gyield()
+    print('m: 2')
+    gyield()
+    print('m: 3')
 
 
 if __name__ == '__main__':
