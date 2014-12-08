b4220682e495f9db1f347ad6a799e801bbe449a0
veegee <veegee@veegee.org>
2014-12-08 04:46:58 -0500

    Add FIXME note

--- examples/cassandra_db.py
+++ examples/cassandra_db.py
@@ -2,7 +2,6 @@ import guv
 
 guv.monkey_patch()
 
-import guv.hubs
 from cassandra.cluster import Cluster
 import logging
 import logger
@@ -12,16 +11,25 @@ log = logging.getLogger()
 
 
 def main():
-    # h = guv.hubs.get_hub()
     nodes = ['192.168.20.2']
 
     cluster = Cluster(nodes, port=9042)
     session = cluster.connect('test')
-    log.info('Execute command')
+    log.info('Execute commands')
     rows = session.execute('SELECT * FROM numbers')
 
     for row in rows:
         log.info(row)
+
+    # FIXME: the current Cassandra support module hangs for unknown reasons, so hit ctrl-c now
+    # see: https://github.com/veegee/guv/issues/10
+    log.warn('The current Cassandra support module hangs for unknown reasons, so hit ctrl-c now')
+    try:
+        guv.sleep(600)
+    except KeyboardInterrupt:
+        pass
+
+    cluster.shutdown()
 
 
 if __name__ == '__main__':
