b3663390255cff2d8c81b1c858f1ba727c02b652
veegee <veegee@veegee.org>
2014-12-09 19:50:10 -0500

    Import and configure logger before guv
    This is done so we are able to see debug messages from the guv patcher.

--- examples/cassandra_db.py
+++ examples/cassandra_db.py
@@ -6,39 +6,34 @@ monkey-patching everything (including the Cassandra driver), making it guv-frien
 
 Adjust this example to your database address, keyspace, and query that you would like to run.
 """
-import guv
+import logger
+logger.configure()
 
+import guv
 guv.monkey_patch()
 
-from cassandra.cluster import Cluster
 import logging
-import logger
 
-logger.configure()
+from cassandra import cluster
+
 log = logging.getLogger()
 
 
 def main():
     nodes = ['192.168.20.2']
 
-    cluster = Cluster(nodes, port=9042)
-    session = cluster.connect('test')
+    c = cluster.Cluster(nodes, port=9042)
+    session = c.connect('test')
     log.info('Execute commands')
     rows = session.execute('SELECT * FROM numbers')
 
     for row in rows:
         log.info(row)
 
-    # FIXME: the current Cassandra support module hangs for unknown reasons, so hit ctrl-c now
-    # see: https://github.com/veegee/guv/issues/10
-    log.warn('The current Cassandra support module hangs for unknown reasons, so hit ctrl-c now')
-    try:
-        guv.sleep(600)
-    except KeyboardInterrupt:
-        pass
-
-    cluster.shutdown()
+    log.warn('Cassandra support is currently incomplete and buggy.')
+    c.shutdown()
 
 
 if __name__ == '__main__':
     main()
+
