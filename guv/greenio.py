5e957e2bfebe2f05e917b4cb68d78c6bd975a756
veegee <veegee@veegee.org>
2014-12-14 14:32:32 -0500

    Remove unnecessary parentheses

--- guv/greenio.py
+++ guv/greenio.py
@@ -3,7 +3,7 @@ import time
 import os
 import _socket
 import errno
-from errno import (EWOULDBLOCK, EBADF)
+from errno import EWOULDBLOCK, EBADF
 
 from . import patcher
 from .hubs import trampoline
